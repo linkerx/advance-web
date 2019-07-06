ee6fa007e77fdac2a725f8f57a38a0f869d6174d
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-07-06 17:12:24 -0300

    contenidos estudios

--- src/views/estudios/ecografia/index.js
+++ src/views/estudios/ecografia/index.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class EstudioEcografia extends React.Component {
     render(){
         return(
             <div className='estudio-ecografia'>
-                Ecografías
+                <div className='wrapper-central'>
+                    <h1>ECOGRAFÍA</h1>
+                    <p>
+                    La ecografía es un método de diagnóstico que utiliza ondas de ultrasonidos
+                    para obtener imágenes de los órganos internos del cuerpo.
+                    Su técnica es sencilla, rápida, segura e indolora.
+                    </p>
+                    <h2>Antes del estudio</h2>
+                    <ul>
+                        <li>El día de la realización de cualquier estudio, deberás presentarte 15
+                        minutos antes del turno en la recepción de Advance.</li>
+                        <li>Traer los últimos estudios previos relacionados a la práctica.</li>
+                        <li>Recordá que este tiempo ha sido reservado para vos. Si no es posible
+                        acudir en día y hora convenidos, por favor avisar anticipadamente.</li>
+                        <li>Ante cualquier duda o inquietud, contactanos​ (link a “Contacto”).</li>
+                    </ul>
+                    <h2>Tipo de exámenes</h2>
+                    <ul>
+                        <li>Generales​ (listado desplegable)</li>
+                        <li>Ecoddopler ​(listado desplegable)</li>
+                        <li>Especiales ​(listado desplegable)</li>
+                        <li>Intervencionismo guiado por ecografía ​(listado desplegable)</li>
+                    </ul>
+                    <h2>Preguntas frecuentes</h2>
+                    <ul>
+                        <li>¿Quién debe interpretar una ecografía? ( ​ respuesta desplegable)</li>
+                        <li>¿Qué es la ecografía Ecoddopler? ¿Qué permite diagnosticar?
+                            (respuesta desplegable)
+                            <div>Es un tipo especial de ecografía que permite examinar el estado del
+                                corazón, las arterias y de las venas. Con ella se evalúa: función
+                                cardíaca, velocidad de flujo sanguíneo, calibre, permeabilidad,
+                                obstrucción, etc. El método de realización es el mismo que el de la
+                                ecografía convencional.</div>
+                        </li>
+                        <li>¿Qué diferencia hay entre una ecografía 2D, 3D, 4D y 5D?​ ​(respuesta
+                            desplegable)</li>
+                        <li>¿En qué mes del embarazo debe realizarse una ecografía? ¿Qué
+                            permite diagnosticar?</li>
+                        <li>¿A partir de qué mes de embarazo se puede identificar el sexo del
+                            bebé a través de una ecografía? ( ​ respuesta desplegable)</li>
+                        <li>¿En qué mes se pueden detectar patologías en el bebé? ​(respuesta
+                            desplegable)</li>
+                        <li>¿En qué mes se pueden detectar patologías en el bebé? ​(respuesta
+                            desplegable)</li>
+                        <li>¿Cómo se debe preparar a la madre para una ecografía? ​(respuesta
+                            desplegable)</li>
+                        <li>¿En qué casos puede fallar una ecografía? ( ​ respuesta desplegable)</li>
+                   </ul>
+                </div>
             </div>
         )
     }
--- src/views/estudios/resonancia/index.js
+++ src/views/estudios/resonancia/index.js
@@ -62,109 +62,95 @@ class EstudioResonancia extends React.Component {
                     </ul>
 
                     <h2>Preguntas frecuentes</h2>
-                    
-                    
-                    
-                    
-                    ●
-                    ¿Qué sucede durante el procedimiento? ​(respuesta desplegable)
-                    Durante el examen el paciente será monitoreado y observado por
-                    cámaras de video. Además, tendrá en su mano un timbre que lo podrá
-                    utilizar para comunicarse con el técnico responsable de realizar el
-                    estudio.
-                    ●
-                    ¿Es peligroso o produce dolor realizarse una resonancia magnética?
-                    (respuesta desplegable)
-                    La resonancia magnética es un estudio totalmente indoloro. Simplemente el
-                    paciente debe permanecer inmóvil dentro del equipamiento, en forma
-                    horizontal.
-                    Los campos magnéticos no producen ningún daño conocido a los tejidos al no
-                    tratarse de radiaciones ionizantes tipo rayos X, así que no emite ningún tipo de
-                    radiación peligrosa, ni surgen complicaciones posteriores.
-                    ●
-                    ¿El estudio es ruidoso? ( ​ respuesta desplegable)
-                    La máquina emite un ruido fuerte como de martilleo o golpes en algunas fases
-                    que es totalmente normal, por lo que el paciente no debe asustarse o
-                    preocuparse por ello.
-                    ●
-                    ¿Qué pasa si soy claustrofóbico? ​(respuesta desplegable)
-                    Al tener el resonador magnético forma de túnel, cualquier persona puede
-                    experimentar cierta claustrofobia. Si sos propenso a sufrir este problema,
-                    previo al estudio el médico podría administrarte un tranquilizante.
-                    ●
-                    ¿Qué sucede si tengo piercing, prótesis, tatuaje, ortodoncia o
-                    marcapasos? ¿Qué metales son peligrosos o están contraindicados?
-                    (respuesta desplegable)
-                    Los ​artículos peligrosos para la salud o que ​podrían causar otros problemas
-                    durante una resonancia magnética son:
-                    -
-                    -
-                    -
-                    Marcapasos cardíaco o desfibrilador implantable
-                    Sujetador metálico puesto para evitar el sangrado en un
-                    aneurisma intracraneal
-                    Catéter con componentes metálicos que pueden tener el
-                    riesgo de producir quemaduras
-                    -
-                    -
-                    Bomba para medicamentos (por ejemplo, las utilizadas para
-                    administrar insulina o analgésicos)
-                    Implante coclear (oído interno)
-                    Los objetos que ​podrían interferir un estudio de ​resonancia magnética ​y, por
-                    lo tanto, están ​c
-                    ontraindicados​ son:
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    -
-                    Presencia de grapas quirúrgicas.
-                    Prótesis ortopédicas, de oído, de globo ocular, dental,
-                    vascular, biliar, cardiaca.
-                    Suturas o catéteres metálicos.
-                    Presencia de dispositivo intrauterino o diafragma.
-                    Existencia en el interior del cuerpo de trozos de balas,
-                    perdigón, metralla, u algún tipo de esquirla metálica ocular y
-                    orbitaria.
-                    Válvulas de derivación.
-                    Filtros vasculares.
-                    Alambres de embolización.
-                    Presencia de neuroestimulador.
-                    Existencia de tatuaje.
-                    Presencia de estimulador de crecimiento óseo.
-                    Los ​empastes dentales metálicos y los ​aparatos de ortodoncia en general no
-                    son afectados por el campo magnético, pero sí pueden distorsionar las
-                    imágenes de la cara o el cerebro.
-                    ●
-                    ¿Las embarazadas pueden realizarse resonancias? ​(respuesta
-                    desplegable)
-                    Las embarazadas o las mujeres que sospechen estar embarazadas deberán
-                    informarlo a Advance al solicitar el turno, o al médico durante el
-                    procedimiento de verificación que se lleva a cabo antes de comenzar el
-                    estudio. En general, no hay riesgos conocidos de la resonancia magnética en
-                    mujeres embarazadas. Sin embargo, en pacientes embarazadas, este tipo de
-                    estudio se reserva sólo para problemas muy serios o anomalías sospechadas.
-                    En todo caso, es muy probable que la resonancia magnética sea menos
-                    peligrosa para el feto que los rayos X o la tomografía computada (TC).
-                    ●
-                    ¿Las mujeres en período de lactancia pueden realizarse resonancias?
-                    (respuesta desplegable)
-                    Las mujeres que se encuentren en período de lactancia -es decir, que estén
-                    amamantando- deben informarlo a Advance al solicitar el turno. Esto es
-                    particularmente importante si es necesaria la inyección de un material de
-                    contraste para realizar la resonancia. En ese caso, una opción es sacarse la
-                    leche antes del estudio, que puede ser utilizada para alimentar al bebé hasta
-                    que se haya eliminado el medio de contraste del cuerpo. Generalmente, eso
-                    demora unas 24 horas. El médico proporcionará a cada paciente información
-                    adicional con respecto a este tema.
-
-
+                    <ul>
+                        <li>¿Qué sucede durante el procedimiento? ​(respuesta desplegable)
+                            <div>Durante el examen el paciente será monitoreado y observado por
+                            cámaras de video. Además, tendrá en su mano un timbre que lo podrá
+                            utilizar para comunicarse con el técnico responsable de realizar el
+                            estudio.</div>
+                        </li>
+                        <li>¿Es peligroso o produce dolor realizarse una resonancia magnética?
+                        (respuesta desplegable)
+                            <div>La resonancia magnética es un estudio totalmente indoloro. Simplemente el
+                            paciente debe permanecer inmóvil dentro del equipamiento, en forma
+                            horizontal.
+                            Los campos magnéticos no producen ningún daño conocido a los tejidos al no
+                            tratarse de radiaciones ionizantes tipo rayos X, así que no emite ningún tipo de
+                            radiación peligrosa, ni surgen complicaciones posteriores.</div>
+                        </li>
+                        <li>¿El estudio es ruidoso? ( ​ respuesta desplegable)
+                            <div>La máquina emite un ruido fuerte como de martilleo o golpes en algunas fases
+                            que es totalmente normal, por lo que el paciente no debe asustarse o
+                            preocuparse por ello.
+                            </div>
+                        </li>
+                        <li>¿Qué pasa si soy claustrofóbico? ​(respuesta desplegable)
+                            <div>Al tener el resonador magnético forma de túnel, cualquier persona puede
+                            experimentar cierta claustrofobia. Si sos propenso a sufrir este problema,
+                            previo al estudio el médico podría administrarte un tranquilizante.</div>
+                        </li>
+                        <li>¿Qué sucede si tengo piercing, prótesis, tatuaje, ortodoncia o
+                            marcapasos? ¿Qué metales son peligrosos o están contraindicados?
+                            (respuesta desplegable)
+                            <div>Los ​artículos peligrosos para la salud o que ​podrían causar otros problemas
+                                durante una resonancia magnética son:
+                                <ul>
+                                    <li>Marcapasos cardíaco o desfibrilador implantable</li>
+                                    <li>Sujetador metálico puesto para evitar el sangrado en un
+                                    aneurisma intracraneal</li>
+                                    <li>Catéter con componentes metálicos que pueden tener el
+                                    riesgo de producir quemaduras</li>
+                                    <li>Bomba para medicamentos (por ejemplo, las utilizadas para
+                                    administrar insulina o analgésicos)</li>
+                                    <li>Implante coclear (oído interno)</li>
+                                </ul>
+                                Los objetos que ​podrían interferir un estudio de ​resonancia magnética ​y, por
+                                lo tanto, están ​contraindicados​ son:
+                                <ul>
+                                    <li>Presencia de grapas quirúrgicas.</li>
+                                    <li>Prótesis ortopédicas, de oído, de globo ocular, dental,
+                                    vascular, biliar, cardiaca.</li>
+                                    <li>Suturas o catéteres metálicos.</li>
+                                    <li>Presencia de dispositivo intrauterino o diafragma.</li>
+                                    <li>Existencia en el interior del cuerpo de trozos de balas,
+                                    perdigón, metralla, u algún tipo de esquirla metálica ocular y
+                                    orbitaria.</li>
+                                    <li>Válvulas de derivación.</li>
+                                    <li>Filtros vasculares.</li>
+                                    <li>Alambres de embolización.</li>
+                                    <li>Presencia de neuroestimulador.</li>
+                                    <li>Existencia de tatuaje.</li>
+                                    <li>Presencia de estimulador de crecimiento óseo.</li>
+                                    <li>Los ​empastes dentales metálicos y los ​aparatos de ortodoncia en general no
+                                    son afectados por el campo magnético, pero sí pueden distorsionar las
+                                    imágenes de la cara o el cerebro.</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>¿Las embarazadas pueden realizarse resonancias? ​(respuesta
+                            desplegable)
+                            <div>Las embarazadas o las mujeres que sospechen estar embarazadas deberán
+                                informarlo a Advance al solicitar el turno, o al médico durante el
+                                procedimiento de verificación que se lleva a cabo antes de comenzar el
+                                estudio. En general, no hay riesgos conocidos de la resonancia magnética en
+                                mujeres embarazadas. Sin embargo, en pacientes embarazadas, este tipo de
+                                estudio se reserva sólo para problemas muy serios o anomalías sospechadas.
+                                En todo caso, es muy probable que la resonancia magnética sea menos
+                                peligrosa para el feto que los rayos X o la tomografía computada (TC).</div>
+                        </li>
+                        <li>¿Las mujeres en período de lactancia pueden realizarse resonancias?
+                            (respuesta desplegable)
+                            <div>Las mujeres que se encuentren en período de lactancia -es decir, que estén
+                                amamantando- deben informarlo a Advance al solicitar el turno. Esto es
+                                particularmente importante si es necesaria la inyección de un material de
+                                contraste para realizar la resonancia. En ese caso, una opción es sacarse la
+                                leche antes del estudio, que puede ser utilizada para alimentar al bebé hasta
+                                que se haya eliminado el medio de contraste del cuerpo. Generalmente, eso
+                                demora unas 24 horas. El médico proporcionará a cada paciente información
+                                adicional con respecto a este tema.
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
