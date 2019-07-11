fc741a430cddbdbce26b8a0de55fa7e4ab9ff632
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-07-11 14:16:10 -0300

    imagenes para slider

--- src/views/home/inicio/slider/index.js
+++ src/views/home/inicio/slider/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
+import WpSlider from './slider';
 import './styles.scss';
 
 class HomeSlider extends React.Component {
     render() {
         return (
             <section id='home-slider'>
-                <div className='slider-container'>
-                    <div className='image'></div>
-                    <div className='texto'>
-                        <h2>Tu salud en buenas manos</h2>
-                        <p>
-                            Trabajamos con la mejor tecnología y un equipo médico
-                            especializado en diagnóstico por imágenes para brindarte la atención que buscás.
-                        </p>
-                        
-                        <a className='btn'>Conocenos</a>
-                        
-                    </div>
-                </div>
+                <WpSlider />
             </section>
         )
     }
