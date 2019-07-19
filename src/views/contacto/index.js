569227a21b4b790ef5c22c8211b1ef7b2bb338b1
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-07-18 22:07:42 -0300

    mejora contacto

--- src/views/contacto/index.js
+++ src/views/contacto/index.js
@@ -39,19 +39,33 @@ class Contacto extends React.Component {
                 <div className='der'>
 
                 <h3>Horarios de atención</h3>
-                <p>
-                Lunes a viernes 8.00 a 20.00
-                Sábado Cerrado
-                Domingo Cerrado
-                </p>
-
-                ícono 02920 429957
-                ícono info@diagnosticoadvance.com
-                ícono Alem 1360, Viedma, Río Negro, Argentina
+                <div className='horarios'>
+                            <div className='linea'></div>
+                            <span>
+                                Lunes a viernes - 08.00 a 20.00<br/>
+                                Sábado - Cerrado<br/>
+                                Domingo - Cerrado<br/>
+                            </span>
+                            <div className='linea'></div>
+                </div>
+                <div className='info'>
+                        <div className='ubicacion'>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>Alem 1360, Viedma, Río Negro</span>
+                        </div>
+                        <div className='email'>
+                            <i className="fas fa-envelope"></i>
+                            <span><a href='mailto:info@diagnosticoadvance.com​'>info@diagnosticoadvance.com</a></span>
+                        </div>
+                        <div className='telefono'>
+                            <i className="fas fa-phone"></i>
+                            <span><a href='tel:+54 02920 429957'>+54 02920 429957</a></span>
+                        </div>
+                    </div>
 
                 </div>
                 </div>
-
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.2060205075977!2d-62.98765804860231!3d-40.82343821981182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95f6984163f20bfd%3A0xad8863410e623f44!2sAlem+1360%2C+Viedma%2C+R%C3%ADo+Negro!5e0!3m2!1ses-419!2sar!4v1563498177703!5m2!1ses-419!2sar" width="100%" height="500" frameborder="0" allowfullscreen></iframe>
                 </div>
             </section>
         )
