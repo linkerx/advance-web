dc307ebb59016c2d11614d8916a8e29f2f7af2ad
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2020-06-01 11:09:14 -0300

    logos obras sociaales

--- src/views/obras-sociales/index.js
+++ src/views/obras-sociales/index.js
@@ -9,19 +9,19 @@ class ObrasSociales extends React.Component {
                 <div className='wrapper-central'>
                     <p className='subtitulo'>Trabajamos con las siguientes obras sociales, prepagas y mutuales. Estamos ampliando nuestra cartilla. Si tenés dudas, contactanos.</p>
                     <ul>
-                        <li>ACA SALUD</li>
-                        <li>FEDERADA SALUD</li>
-                        <li>HORIZONTE</li>
-                        <li>IPROSS</li>
-                        <li>LA SEGUNDA</li>
-                        <li>OSAM</li>
-                        <li>OSDOP</li>
-                        <li>OSPECON</li>
-                        <li>OSPIA</li>
-                        <li>OSPM</li>
-                        <li>OSUTHGRA</li>
-                        <li>PREVENCION ART</li>
-                        <li>SANCOR SALUD</li>
+                        <li><img src='/images/osoc/acasalud.png' /></li>
+                        <li><img src='/images/osoc/federadasalud.png' /></li>
+                        <li><img src='/images/osoc/horizonteseguros.png' /></li>
+                        <li><img src='/images/osoc/ipross.png' /></li>
+                        <li><img src='/images/osoc/lasegunda.png' /></li>
+                        <li><img src='/images/osoc/osam.png' /></li>
+                        <li><img src='/images/osoc/osdop.png' /></li>
+                        <li><img src='/images/osoc/ospecon.png' /></li>
+                        <li><img src='/images/osoc/ospia.png' /></li>
+                        <li><img src='/images/osoc/ospm.jpg' /></li>
+                        <li><img src='/images/osoc/osuthgra.png' /></li>
+                        <li><img src='/images/osoc/prevart.png' /></li>
+                        <li><img src='/images/osoc/sancor.png' /></li>
                     </ul>
 
                 </div>
