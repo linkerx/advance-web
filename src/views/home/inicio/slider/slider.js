de0c22245478dd715c7a315f1321d158daa19529
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2020-10-02 15:07:11 -0300

    cancer de mamas

--- src/views/home/inicio/slider/slider.js
+++ src/views/home/inicio/slider/slider.js
@@ -62,7 +62,7 @@ class WpSlider extends React.Component {
       {
         id: 1,
         img: "/images/slider/mamografia.jpg",
-        texto: "<h3>Servicio de mamografía</h3><div class='linea'></div>Realizá tus estudios de rutina con un equipamiento único en la región. Más comodidad, mejores resultados.",
+        texto: "<img src='/images/lazo-rosa-cancer.png' style='position:absolute;top:-100px;height: 90px;left: -10px;' /><h3>Semana de prevención del Cáncer de mama</h3><div class='linea'></div>Realizá tus estudios de rutina con un equipamiento único en la región. Más comodidad, mejores resultados.",
         link: "/estudios/mamografia",
         textoLink: "Ver más"
       },
